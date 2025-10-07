095f87d29e00c23735a056a1ceb49432b81dc873
BROGI:) <ea0399596@gmail.com>
2025-10-06 21:21:56 -0500

    Delete Evidencia-de-conocimientos directory

--- Evidencia-de-conocimientos/ENTREGABLE 2/securesys/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from views.app_view import SecureSysApp
-
-def main():
-    app = SecureSysApp()  # 👈 sigue heredando de tk.Tk
-    app.mainloop()
-
-if __name__ == "__main__":
-    main()
